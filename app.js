8751f9e5581b07def9f83ea7a618f9a021a6a6d2
ElisePatrikainen <elise.patrikainen@gmail.com>
2019-11-07 20:12:16 +0100

    (refactorisation)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const exemple = require('./events/eventEmittter_exemple');
+const exemple = require('./events/customEventEmittter');
 
 exemple.timer.on('3_secondes', () => console.log('3 secondes écoulées.'));
 exemple.launchTimer();
