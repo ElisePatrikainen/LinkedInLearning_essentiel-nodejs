0f5409eaaa75199b64e736871e34c16cd97fabc9
ElisePatrikainen <elise.patrikainen@gmail.com>
2019-11-07 11:52:08 +0100

    Exemple: écrire un fichier

---
+++ async-io_example.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+function generateData() {
+    let data = '1';
+    for (i=0; i < 1000000; i++) {
+        data = data + Math.random() * 10;
+    }
+    return data;
+}
+
+function callback() {
+    console.log('Process \'bigFile\' terminé, durée ' + (Date.now() - date) + ' ms.')
+}
+
+fs.writeFile('bigFile', generateData(), callback)
+const date = Date.now();
+console.log('Disponible.');
