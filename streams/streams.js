5844ddbe5dfebcd0389b016ffc421c074c8aac14
ElisePatrikainen <elise.patrikainen@gmail.com>
2019-11-08 15:58:05 +0100

    Chaîner les streams (2) : méthode pipe

--- streams/streams.js
+++ streams/streams.js
@@ -2,8 +2,5 @@ const fs = require('fs');
 const writeFileStream = fs.createWriteStream('../bigFile');
 
 process.stdin.setEncoding('utf8');
-process.stdin.on("data", pipeToWriteStream)
 
-function pipeToWriteStream(input) {
-    writeFileStream.write(input);
-}
+process.stdin.pipe(writeFileStream);
