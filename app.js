56f8e329c155e962fed856c4c28fa95d8e1c0d55
ElisePatrikainen <elise.patrikainen@gmail.com>
2019-11-10 14:43:36 +0100

    Intégrer MongoDB : utiliser le driver

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const url = require('url');
 const express = require('express');
 const app = express();
+const mongoClient = require('mongodb');
+
 require('./router').initRouter(app);
+
+mongoClient.connect(
+    "mongodb://localhost/test", 
+    {useUnifiedTopology: true}, 
+    require('./handlers/mongoDb').connection
+)
 
 app.listen(8000);
