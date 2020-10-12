05feb531f9a6fb299931b1ead2c77b31b8f5de09
patrikainen <patrikainen.externe@osmos-group.com>
2020-10-12 13:05:36 +0200

    [Correctif] Enchaîner lecture et écriture d'un fichier

--- async-io_example.js
+++ async-io_example.js
@@ -2,23 +2,21 @@ const fs = require('fs');
 
 function generateData() {
     let data = '1';
-    for (i=0; i < 1000000; i++) {
+    for (i=0; i < 10000; i++) {
         data = data + Math.random() * 10;
     }
     return data;
 }
 
-function callback() {
-    console.log('Process \'bigFile\' terminé, durée ' + (Date.now() - date) + ' ms.')
+const date = Date.now();
+
+try {
+    fs.writeFile('bigFile', generateData(), () => {
+        console.log('Ecriture \'bigFile\' terminé, durée ' + (Date.now() - date) + ' ms.')
+        fs.readFile('bigFile', "utf8", () => console.log('Lecture \'bigFile\' terminée'))
+        })
+} catch (e) {
+    console.log(e)
 }
 
-fs.writeFile('bigFile', generateData(), () => callback())
-const date = Date.now();
 console.log('Disponible.');
-
-fs.readFile('bigFile1', (err, data) => {
-    if (err) {
-        throw new Error('Erreur détectée.')
-    }
-    console.log(data)
-})
